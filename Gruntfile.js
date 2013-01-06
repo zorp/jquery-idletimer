4354a7a860c0b71a104413c324c220c115c389c0
Mike Sherov <mike.sherov@gmail.com>
2013-01-06 16:45:53 -0500

    make the "watch" grunt task actually work.

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,6 +75,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
